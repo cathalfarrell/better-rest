1ee06e9fa8d5289bfeec46fb3ddc6a60ac734f97
Cathal Farrell <cathalproductions@gmail.com>
2020-04-08 16:49:10 +0100

    Challenge 2 attempt - but abandoned - as picker not working Challenge 3 - now displaying on ui the suggested bedtime

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -18,6 +18,34 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var isShowingAlert = false
 
+    var suggestedBedtime: String {
+
+        let model = SleepCalculator()
+
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+
+        do {
+            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: coffeeAmount)
+
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            //to convert to nicely readable time:
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+
+            return formatter.string(from: sleepTime)
+
+        } catch let err {
+
+            print("Error: \(err)")
+
+            return "Error"
+        }
+
+    }
+
     var body: some View {
         NavigationView {
             Form {
@@ -27,7 +55,7 @@ struct ContentView: View {
                          .datePickerStyle(WheelDatePickerStyle())//shows it on form layout
                 }
                 .font(.headline)
-                
+
                 Section(header: Text("Desired amount of sleep?")){
                     Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
                         Text("\(sleepAmount, specifier: "%g") hours")
@@ -35,7 +63,7 @@ struct ContentView: View {
                 }
                 .font(.headline)
 
-                Section(header: Text("Daily coffee intake?")){
+                Section(header: Text("Daily coffee intake? - Stepper")){
                     Stepper(value: $coffeeAmount, in: 1...20, step: 1){
                         if coffeeAmount == 1{
                             Text("1 cup")
@@ -47,6 +75,38 @@ struct ContentView: View {
                 }
                 .font(.headline)
 
+                Section(header: Text("Suggested Bedtime")){
+                   Text(suggestedBedtime)
+                }
+                .font(.headline)
+
+
+                /* ------------------------------------------
+
+                /*
+                Challenge 2 - couldnt get this picker to work
+                Replace the “Number of cups” stepper with a Picker showing the same range of values.
+                */
+
+                Section(header: Text("Daily coffee intake? - Picker")){
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            if $0 > 1 {
+                                Text("\($0) cups")
+                            } else {
+                                Text("1 cup")
+                            }
+
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    .labelsHidden()
+                }
+                .font(.headline)
+
+                Text("You chose: Cups # \(coffeeAmount)")
+
+             --------------------------------------------- */
             }
             .navigationBarTitle("BetterRest")
             .navigationBarItems(trailing:
